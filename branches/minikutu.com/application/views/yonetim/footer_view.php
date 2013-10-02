538070b993a4c694080157a9a17a3ef82621ef59
kochserkan@gmail.com <kochserkan@gmail.com@959991d6-d349-f033-2a96-8549cfca7452>
2013-10-02 07:50:42 +0000

    admin footer düzenleme

--- branches/minikutu.com/application/views/yonetim/footer_view.php
+++ branches/minikutu.com/application/views/yonetim/footer_view.php
@@ -11,6 +11,6 @@ if(!defined('BASEPATH'))
 </div>
 </div>
 <div id="footer">
-	<a href="#">E-Ticaret Sistemim</a> © 2007-<?php echo date('Y'); ?> All Rights Reserved. | <a href="#">E-Ticaret</a> © 2007-<?php echo date('Y'); ?> Tüm Hakları Saklıdır.<br>Version <?php echo ETICARET_VERSION; ?>
+	<a href="#">© 2013 Marsma E-Commerce. Tüm Hakları Saklıdır.</a><br>Version <?php echo ETICARET_VERSION; ?>
 </div>
 </body></html>
