336af7f5920c36aab12b52480475c3b3a7e9b2ec
kochserkan@gmail.com <kochserkan@gmail.com@959991d6-d349-f033-2a96-8549cfca7452>
2013-11-08 07:19:54 +0000

    xml düzenleme

--- branches/minikutu.com/application/controllers/yonetim/api.php
+++ branches/minikutu.com/application/controllers/yonetim/api.php
@@ -11,6 +11,11 @@ if (!defined('BASEPATH')) {
 class api extends Admin_Controller
 {
 
+	public $file = array(
+		"categories" => "http://www.technomodel.com/xml/dealer/categories.xml?dId=480&k=6earD4KOoJaQpyzb",
+		"products" => "http://www.technomodel.com/xml/dealer/products.xml?dId=480&k=6earD4KOoJaQpyzb"
+	);
+
 	public function __construct() {
 		parent::__construct();
 		log_message('debug', 'api');
@@ -20,8 +25,8 @@ class api extends Admin_Controller
 	}
 
 	public function addCategories() {
-		$file = "http://www.technomodel.com/xml/dealer/categories.xml?dId=480&k=6earD4KOoJaQpyzb";
-		$categories = @simplexml_load_file($file);
+
+		$categories = @simplexml_load_file($this->file['categories']);
 		if ($categories->children()->count() > 0) {
 			foreach ($categories->children() as $category) {
 				$this->get_nodes($category); // <category></category>
@@ -46,111 +51,111 @@ class api extends Admin_Controller
 	}
 
 	public function addProducts() {
-		$file = "http://www.technomodel.com/xml/dealer/products.xml?dId=480&k=6earD4KOoJaQpyzb";
-		$products = @simplexml_load_file($file);
+
+		$products = @simplexml_load_file($this->file['products']);
 		if ($products) {
-			foreach ($products->children() as $urun) {
+			foreach ($products->children() as $product) {
 
+				$urun_control = $this->product_product_model->xml_product_control((string) $product->id);
 
-				//p($returnNames);
-				//exit;
-				//exit;
-				// download main image
-				//echo $urun->images->image[0]; exit;
-				$new_image_name = $this->downloadImages($urun->images->image[0], $urun->name);
+				if (!$urun_control) {
 
-				//echo $urun->desi; exit;
-				$olcu = pow($urun->desi * 3000,1/3); // width, height,length
-				$olcu = number_format($olcu, 2, '.', '');
-				//echo $olcu; 
-				//exit;
-				//price type
-				$price_type = 1;
-				if ($urun->currency == 'Dolar')
-					$price_type = 2;
-				else if ($urun->currency == 'Euro')
-					$price_type = 3;
+					$ilk_img_path = $product->xpath("images/image[@main = '1']");
+					$new_image_name = $this->downloadImages($ilk_img_path[0], $product->name);
 
-				$urunData = array(
-					"model" => (string) $urun->id,
-					"quantity" => $urun->stock,
-					"stock_status_id" => 0,
-					"image" => $new_image_name,
-					"price" => $urun->price,
-					"price_type" => $price_type,
-					"stock_type" => 12,
-					"tax" => $urun->kdv,
-					"date_available" => time() + 30 * 24 * 60 * 60, // default 1 ay
-					"status" => 1,
-					"feature_status" => 0,
-					"cargo_required" => 1,
-					"date_added" => time(),
-					"date_modified" => time(),
-					"length" => $olcu,
-					"width" => $olcu,
-					"height" => $olcu,
-				);
-				$inserted_id = $this->product_product_model->Ekle($urunData, "product");
+					$WHL = pow($product->desi * 3000, 1 / 3); // width, height,length
+					$WHL = number_format($WHL, 2, '.', '');
 
-				$urunDesc = array(
-					"product_id" => $inserted_id,
-					"language_id" => 1,
-					"seo" => url_title($urun->name, "dash", TRUE),
-					"name" => (string) $urun->name,
-					"description" => (string) $urun->description
-				);
-				$this->product_product_model->Ekle($urunDesc, "product_description");
-				// diğer image işlemleri
-				$i = 1;
-				$returnNames = array();
-				if ($urun->images->children()->count() > 0) {
-					foreach ($urun->images->children() as $image) {
-						if ($i != 1) {
-							$returnNames[] = $this->downloadImages($image, $urun->name . "_" . $i);
-						}
-						++$i;
-					}
-				}
-				// diğer image son
+					$price_type = 1;
+					if ($product->currency == 'Dolar')
+						$price_type = 2;
+					else if ($product->currency == 'Euro')
+						$price_type = 3;
 
-				if ($returnNames) {
-					foreach ($returnNames as $path) {
-						$urunImgData = array(
-							"product_id" => $inserted_id,
-							"image" => (string) $path
-						);
-						$this->product_product_model->Ekle($urunImgData, "product_image");
-					}
-				}
-				// category işlemleri
-				if ($urun->categories->children()->count() > 0) {
-
-					// rc model ile ilişkilendirme
-					$urunProductToCategory = array(
-						"product_id" => $inserted_id,
-						"category_id" => 177
+					$urunData = array(
+						"model" => (string) $product->id,
+						"quantity" => $product->stock,
+						"stock_status_id" => 0,
+						"image" => $new_image_name,
+						"price" => $product->price,
+						"price_type" => $price_type,
+						"stock_type" => 12,
+						"tax" => $product->kdv,
+						"date_available" => time() + 30 * 24 * 60 * 60, // default 1 ay
+						"status" => 1,
+						"feature_status" => 0,
+						"cargo_required" => 1,
+						"date_added" => time(),
+						"date_modified" => time(),
+						"length" => $WHL,
+						"width" => $WHL,
+						"height" => $WHL,
 					);
-					$this->product_product_model->Ekle($urunProductToCategory, "product_to_category");
-					foreach ($urun->categories->children() as $category) {
-						//echo $category;
-						$urunProductToCategory = array(
-							"product_id" => $inserted_id,
-							"category_id" => (int) $category
-						);
-						$this->product_product_model->Ekle($urunProductToCategory, "product_to_category");
-					}
-				}
+					$inserted_id = $this->product_product_model->Ekle($urunData, "product");
 
-				echo "başarılı";
-				exit;
+					$urunDescData = array(
+						"product_id" => $inserted_id,
+						"language_id" => 1,
+						"seo" => url_title($product->name, "dash", TRUE),
+						"name" => (string) $product->name,
+						"description" => (string) $product->description
+					);
+					$this->product_product_model->Ekle($urunDescData, "product_description");
+					// diğer image işlemleri
+					$i = 1;
+					$returnNames = array();
+					if ($product->images->children()->count() > 0) {
+						foreach ($product->images->children() as $image) {
+							if ($image->attributes()->main != 1)
+								$returnNames[] = $this->downloadImages($image, $product->name . "_" . $i);
+							++$i;
+						}
+					}
+
+					if ($returnNames) {
+						foreach ($returnNames as $path) {
+							$urunImgData = array(
+								"product_id" => $inserted_id,
+								"image" => (string) $path
+							);
+							$this->product_product_model->Ekle($urunImgData, "product_image");
+						}
+					}
+					// diğer image son
+					// category işlemleri
+					if ($product->categories->children()->count() > 0) {
+
+						// rc model ile ilişkilendirme
+						$urunProductToCategoryData = array(
+							"product_id" => $inserted_id,
+							"category_id" => 177
+						);
+						$this->product_product_model->Ekle($urunProductToCategoryData, "product_to_category");
+						foreach ($product->categories->children() as $category) {
+							$urunProductToCategoryData = array(
+								"product_id" => $inserted_id,
+								"category_id" => (int) $category
+							);
+							$this->product_product_model->Ekle($urunProductToCategoryData, "product_to_category");
+						}
+					}
+
+					echo "başarılı";
+					//exit;
+				}
 			}
 		}
 	}
 
-	public function downloadImages($_im_url, $name) {
-		$img = @file_get_contents($_im_url);
+	public function downloadImages($_im_url = "http://demo.minikutu.com", $name = "no-name") {
+		$headers = @get_headers($_im_url, 1);
+		if ($headers["Content-Type"] != "image/jpeg") {
+			return "no-image.jpg";
+		}
+		@file_get_contents($_im_url);
 		$new_image_name = "data/xml_resimleri/" . url_title($name) . ".jpg";
 		@file_put_contents(DIR_IMAGE . $new_image_name, $img);
+
 		return $new_image_name;
 	}
 
