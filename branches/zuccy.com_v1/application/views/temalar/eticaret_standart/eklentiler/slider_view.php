82fed386771f6a009d3250aeeda297737aa514f8
kochserkan@gmail.com <kochserkan@gmail.com@959991d6-d349-f033-2a96-8549cfca7452>
2014-06-27 19:33:29 +0000

    nivo to flexslider

--- branches/zuccy.com_v1/application/views/temalar/eticaret_standart/eklentiler/slider_view.php
+++ branches/zuccy.com_v1/application/views/temalar/eticaret_standart/eklentiler/slider_view.php
@@ -3,30 +3,33 @@
 		$slider_sorgu = $this->eklentiler_slider_model->slider_listele();
 		if($slider_sorgu->num_rows()) {
 	?>
-	<link media="screen" rel="stylesheet" type="text/css" href="<?php echo site_js(); ?>nivo/nivo_slider.css" />
-	<link media="screen" rel="stylesheet" type="text/css" href="<?php echo site_js(); ?>nivo/custom.nivo.css" />
-	<script type="text/javascript" src="<?php echo site_js(); ?>nivo/nivo.min.js" charset="utf-8"></script>
+	<link media="screen" rel="stylesheet" type="text/css" href="<?php echo site_js(); ?>flexslider/flexslider.css" />
+	<script type="text/javascript" src="<?php echo site_js(); ?>flexslider/jquery.flexslider.js" charset="utf-8"></script>
 	<script type="text/javascript" charset="utf-8">
-		$(window).load(function() {
-			$('#slider_<?php echo $yer; ?>').nivoSlider({
-				controlNav : false
-			});
-		});
+        $(window).load(function() {
+            $('.flexslider').flexslider({
+                animation: "slide"
+            });
+        });
 	</script>
 	<?php
 		$width = ($yer == 'anasayfa' || $yer == 'slider') ? '821' : '980';
 		$height = ($yer == 'anasayfa' || $yer == 'slider') ? '474' : '314';
 	?>
-	<div id="slider_<?php echo $yer; ?>" style="width:<?php echo $width; ?>px;height:<?php echo $height; ?>px;" class="dynamic_slider nivoSlider">
+	<div id="slider_<?php echo $yer; ?>" style="width:<?php echo $width; ?>px;height:<?php echo $height; ?>px;" class="flexslider">
+        <ul class="slides">
 		<?php foreach($slider_sorgu->result() as $row) { ?>
 		<?php $link = ($row->slider_link) ? $row->slider_link : 'javascript:;'; ?>
+        <li>
 		<a href="<?php echo $link; ?>">
 		<?php
 			$resim = show_image($row->slider_img, $width, $height);
 		?>
-			<img style="display:none;" src="<?php echo $resim; ?>" alt="Slider" />
+			<img src="<?php echo $resim; ?>" alt="Slider" />
 		</a>
+        </li>
 		<?php } ?>
+        </ul>
 	</div>
 	<?php } ?>
 <?php } ?>
